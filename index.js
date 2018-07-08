a6dff6007e7dd4ed94cfc06977e39445e86ffb1b
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-07-08 19:39:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === null) {
+  if (cardNumber == null) {
     return 'Sorry, we don\'t have a credit card on file for you.';
   } else { 
     var totalCost = total();
