163e3171292ac7fafc9f812f81b87cba91b75583
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-07-08 16:44:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  price = Math.floor((Math.random() * 100) + 1);
+  var price = Math.floor((Math.random() * 100) + 1);
   cart.push({itemName: item}, {itemPrice: price});
 }
 
